b5b8b6e6268e4815aac4177b4f1368ce5da7de60
Patrick Schulte <pschulte@inovex.de>
2019-03-07 14:56:11 +0100

    Initial Commit
    Signed-off-by: Patrick Schulte <pschulte@inovex.de>

---
+++ DDPG Cartpole Upswing/ddpg_upswing.py
@@ -0,0 +1,100 @@
+import sys
+sys.path.insert(0, '../')
+sys.dont_write_bytecode = True
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+import numpy as np
+import gym
+import tensorflow as tf
+
+from agents import DDPG_Agent, Random_Agent
+from networks import ActorNetwork, CriticNetwork
+from memory import Memory
+from explorationnoise import UniformNoise
+from environment import Environment
+
+
+########################################################################
+########################################################################
+
+# Name of the OpenAI Gym Environment
+PROBLEM = 'CartpoleDownwardsCont-v0'
+
+############################# Hyperparameter ###########################
+# Learning rate of the actor
+ACTOR_LEARNING_RATE = 0.0001
+
+# Learning rate of critic
+CRITIC_LEARNING_RATE = 0.001
+
+# Batch size
+BATCH_SIZE = 64
+
+# Maximum number of episodes
+MAX_EPISODES = 100000
+
+# warmup steps.
+WARMUP_STEPS = 10000
+
+# Exploration duration
+EXPLORATION_EPISODES = 500
+
+# Discount factor
+GAMMA = 0.99
+
+# Soft target update parameter
+TAU = 0.001
+
+# Size of replay buffer
+BUFFER_SIZE = 1000000
+
+
+
+########################################################################
+########################################################################
+
+
+def main(_):
+    # Create a tensorflow session
+    with tf.Session() as sess:
+
+        # Create the environment
+        environment = Environment(problem = PROBLEM, max_episodes = MAX_EPISODES, warm_up_steps = WARMUP_STEPS, rendering = True)
+
+        # Transfer the dimensions of the state space and the action space into local variables
+        state_dim = environment.env.observation_space.shape
+        action_dim = 1
+
+        # Set the action boundary
+        action_boundary = 10.0
+
+        # Model the network architecture for actor
+        actor = ActorNetwork(sess, state_dim, action_dim, action_boundary, batch_size = BATCH_SIZE,
+                             learning_rate = ACTOR_LEARNING_RATE, tau = TAU)
+
+        # Model the network architecture for critic
+        critic = CriticNetwork(sess, state_dim, action_dim, action_boundary,
+                               actor.get_num_trainable_vars(), learning_rate = CRITIC_LEARNING_RATE,
+                               tau = TAU)
+
+
+        # Initialize the memory
+        memory = Memory(capacity = BUFFER_SIZE)
+
+        # Initialize the exploration noise
+        noise = UniformNoise(bound = 20, n_steps_annealing = EXPLORATION_EPISODES)
+
+        # Create the agents
+        agent = DDPG_Agent(actor, critic, memory, action_boundary, GAMMA, BATCH_SIZE, noise = noise)
+        random_agent = Random_Agent(memory, action_boundary)
+
+        # Create the environment
+        environment.solve_environment(sess, agent, random_agent)
+
+
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ My_Environments/cartpole_downwards.py
@@ -0,0 +1,184 @@
+"""
+Classic cart-pole system implemented by Rich Sutton et al.
+Copied from https://webdocs.cs.ualberta.ca/~sutton/book/code/pole.c
+"""
+
+import logging
+import math
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+import time
+
+
+logger = logging.getLogger(__name__)
+
+class CartPoleEnv_Downwards(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second' : 50
+    }
+
+    def __init__(self):
+        self.gravity = 9.8
+        self.masscart = 1.0
+        self.masspole = 0.1
+        self.total_mass = (self.masspole + self.masscart)
+        self.length = 0.5 # actually half the pole's length
+        self.polemass_length = (self.masspole * self.length)
+        self.force_mag = 10.0
+        self.tau = 0.02  # seconds between state updates
+
+        # Angle at which to fail the episode
+        self.theta_threshold_radians = 5 * 2 * math.pi / 360
+        self.x_threshold = 2.4
+
+        # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
+        high = np.array([
+            self.x_threshold * 2,
+            np.finfo(np.float32).max,
+            self.theta_threshold_radians * 2,
+            np.finfo(np.float32).max])
+
+
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(-high, high)
+
+        self._seed(0)
+        self.viewer = None
+        self.state = None
+
+        self.steps_beyond_done = None
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def _step(self, action):
+        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        state = self.state
+        x, x_dot, theta, theta_dot = state
+        if action == 1:
+            force = self.force_mag
+        elif action == 2:
+            force = -self.force_mag
+        elif action == 3:
+           force = 0.666*self.force_mag
+        elif action == 4:
+           force = 0.333*self.force_mag
+        elif action == 5:
+           force = -self.force_mag
+        elif action == 6:
+           force = -0.666*self.force_mag
+        elif action == 7:
+           force = -0.333*self.force_mag
+        else:
+            force = 0
+        #force = self.force_mag if action==1 else -self.force_mag
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
+        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        x  = x + self.tau * x_dot
+        x_dot = x_dot + self.tau * xacc
+        theta = theta + self.tau * theta_dot
+        theta_dot = theta_dot + self.tau * thetaacc
+        self.state = np.array([x,x_dot,theta,theta_dot])
+        terminate = x < -self.x_threshold \
+                    or x > self.x_threshold \
+
+        terminate = bool(terminate)
+        if not terminate:
+
+            theta = (((theta + math.pi) % (2*math.pi)) - math.pi)
+
+            reward = math.cos(theta*0.5) + math.exp(-theta**2) + math.exp(-theta**2 / 0.01)
+
+            if abs(theta) < math.radians(90):
+                reward = reward + math.exp(-x**2)
+
+            #if abs(theta) > math.radians(20):
+            #    reward = reward - 0.001 * abs(x_dot)
+            #    reward = reward + 0.01 * (1 - math.exp(-theta_dot**2))
+
+            #if abs(theta) > math.radians(45) and abs(theta) < math.radians(135):
+            #    reward = reward - 0.5*math.exp(-theta_dot**2)
+                #print(math.exp(-theta_dot**2))
+
+
+            #if abs(theta) < math.radians(15):
+            #    reward = 1
+            #elif abs(theta) > math.radians(90):
+            #    reward = -1
+            #else:
+            #    reward = 0.2
+
+        elif terminate:
+            reward = -1.0
+        else:
+            if self.steps_beyond_done == 0:
+                logger.warning("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
+            self.steps_beyond_done += 1
+            reward = 0.0
+
+
+        return self.state, reward, terminate, {}
+
+    def _reset(self):
+        self.state = (np.random.uniform(low = -2.0, high = 1.5, size = (1,)), 0, math.pi, 0)#self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        self.steps_beyond_done = None
+        return np.array(self.state)
+
+    def _render(self, mode='human', close=False):
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+            return
+
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.x_threshold*2
+        scale = screen_width/world_width
+        carty = 100 # TOP OF CART
+        polewidth = 10.0
+        polelen = scale * 1.0
+        cartwidth = 50.0
+        cartheight = 30.0
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            l,r,t,b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
+            axleoffset =cartheight/4.0
+            cart = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            self.carttrans = rendering.Transform()
+            cart.add_attr(self.carttrans)
+            self.viewer.add_geom(cart)
+            l,r,t,b = -polewidth/2,polewidth/2,polelen-polewidth/2,-polewidth/2
+            pole = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            pole.set_color(.8,.6,.4)
+            self.poletrans = rendering.Transform(translation=(0, axleoffset))
+            pole.add_attr(self.poletrans)
+            pole.add_attr(self.carttrans)
+            self.viewer.add_geom(pole)
+            self.axle = rendering.make_circle(polewidth/2)
+            self.axle.add_attr(self.poletrans)
+            self.axle.add_attr(self.carttrans)
+            self.axle.set_color(.5,.5,.8)
+            self.viewer.add_geom(self.axle)
+            self.track = rendering.Line((0,carty), (screen_width,carty))
+            self.track.set_color(0,0,0)
+            self.viewer.add_geom(self.track)
+
+        if self.state is None: return None
+
+        x = self.state
+        cartx = x[0]*scale+screen_width/2.0 # MIDDLE OF CART
+        self.carttrans.set_translation(cartx, carty)
+        self.poletrans.set_rotation(-x[2])
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array')
